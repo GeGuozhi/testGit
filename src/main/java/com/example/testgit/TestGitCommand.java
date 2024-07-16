aa96336e88907738bc55d2b53b4c194d6938e0b9
GeGuozhi <1462496578@qq.com>
2024-07-16 17:40:39 +0800

    GeGuozhi1只保留main方法

--- src/main/java/com/example/testgit/TestGitCommand.java
+++ src/main/java/com/example/testgit/TestGitCommand.java
@@ -6,14 +6,6 @@ import java.util.HashMap;
  * @author ggz on 2023/10/9
  */
 public class TestGitCommand {
-    /**
-     * master 10:14
-     *
-     * master 10:27
-     *
-     * user2
-     */
-
     public static void main(String[] args) {
         HashMap<String,String> map = new HashMap<String, String>();
         map.put("a","a");
